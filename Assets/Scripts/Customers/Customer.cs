13cbffeeae43767f0dc74e343b7ccb48d097ab01
Dave Murray <138507286+Dave-Murray98@users.noreply.github.com>
2025-10-31 20:21:17 +0000

    Added entities

---
+++ Assets/Scripts/Customers/Customer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Pathfinding;
+
+public class Customer : MonoBehaviour
+{
+
+    [SerializeField] private FollowerEntity followerEntity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
